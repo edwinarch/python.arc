f341d027cc64c91ef48cb9ed13521152a1579dd6
xixigolang <carloswl010608@gmail.com>
2025-11-13 10:52:17 -0600

    agregar random-loteria

--- proyecto/proyectomanual.py
+++ proyecto/proyectomanual.py
@@ -4,6 +4,7 @@ import os #for borrar pantalla
 import time # agrg tiemp 
 import getpass # ocultar el textp o contr
 #from colorama import rojo # color en consola
+import random
 
 # datos
 nombProducto = []
@@ -32,6 +33,7 @@ def main():
     contra = "123"
     contrasegundoplano = "python"
     contrachikycroos = "chiky"
+    contrarandomLoteria = "loteria"
     intentos = 4 #4vece
     while intentos > 0:
         # Ingresar contra
@@ -49,7 +51,7 @@ def main():
         for numero in range(1, 26):
             #Cargando sumulator |falso|
             print("Descargando pack", numero,"/25")
-            time.sleep(0.01)
+            time.sleep(0.02)
                 
         while True:
             time.sleep(1)
@@ -91,7 +93,13 @@ def main():
                     print(ColoresMain.VERDE,"Contraseña correcta. Acceso concedido al plano chiky croos.", ColoresMain.RESET)
                     time.sleep(2)
                     os.system('cls')
-                    chikycroos() # transportar a "chikycroos()                   
+                    chikycroos() # transportar a "chikycroos()  
+                elif entrada == contrarandomLoteria:
+                    print(esp)
+                    print(ColoresMain.VERDE,"Contraseña correcta. Acceso concedido al LOTERIA.", ColoresMain.RESET)
+                    time.sleep(2) 
+                    os.system('cls')
+                    randomLoteria() # trasnport loteria()          
                 else: # contra incorrecta y cuenta
                     
                     intentos -= 1
@@ -265,6 +273,99 @@ def chikycroos():
     print(ColoresMain.AZUL,myworld,ColoresMain.RESET)
     print(esp)
     exit()
+
+def randomLoteria():
+    while True:
+        espacio = " " #chunk
+        print("1. Primer premio ")
+        print("2. Segundo premio ")
+        print("3. Tercer premio ")
+        print("4. Reventado") 
+        print("5. Salir")
+        print(espacio)
+        a = input("Ingresar premio: ")
+        print(espacio)
+        if a=='1':
+            espacio = " " #chunk)
+            print(espacio, espacio,espacio,espacio)
+            print(espacio,espacio)
+            block = "ESTE ES EL PREMIO: "
+            print(espacio,espacio,espacio)
+            print(espacio,espacio,espacio,espacio,espacio)
+            print(espacio,espacio,espacio)
+            print(block, " !PRIMER !")
+            print(espacio,espacio,espacio,espacio)
+            x1 = random.randrange(0, 99)
+            print("EL numero noraml es: ", x1)
+            x2 = random.randint(100, 999)
+            print("numero serie es: ", x2)
+            print(espacio)
+            options = input("Ingresar cualquier letra para regresar al menu:  ")
+            print(espacio)
+            os.system('cls')
+        elif a=='2':
+            espacio = " " #chunk)
+            print(espacio, espacio,espacio,espacio)
+            print(espacio,espacio)
+            block = "ESTE ES EL PREMIO: "
+            print(espacio,espacio,espacio)
+            print(espacio,espacio,espacio,espacio,espacio)
+            print(espacio,espacio,espacio)
+            print(block, " !SEGUNDO !")
+            print(espacio,espacio,espacio,espacio)
+            x1 = random.randrange(0, 99)
+            print("EL numero noraml es: ", x1)
+            x2 = random.randint(100, 999)
+            print("numero serie es: ", x2)
+            print(espacio)
+            options = input("Ingresar cualquier letra para regresar al menu:  ")
+            print(espacio)
+        elif a=='3':
+            espacio = " " #chunk)
+            print(espacio, espacio,espacio,espacio)
+            print(espacio,espacio)
+            block = "ESTE ES EL PREMIO: "
+            print(espacio,espacio,espacio)
+            print(espacio,espacio,espacio,espacio,espacio)
+            print(espacio,espacio,espacio)
+            print(block, " !TERCER !")
+            print(espacio,espacio,espacio,espacio)
+            x1 = random.randrange(0, 99)
+            print("EL numero noraml es: ", x1)
+            x2 = random.randint(100, 999)
+            print("numero serie es: ", x2)
+            print(espacio)
+            options = input("Ingresar cualquier letra para regresar al menu:  ")
+            print(espacio)
+        elif a=='4':
+            espacio = " " #chunk)
+            espaciov2 = "" #chunk)
+            print(espacio, espacio,espacio,espacio)
+            print(espacio,espacio)
+            x1 = random.randrange(1, 3)
+            if x1==1:
+                print(espaciov2)
+                print("ES BOLA ES! ROJO! ")
+                print(espacio, espaciov2)
+            elif x1==2:
+                print(espaciov2,espaciov2)
+                print("ES BOLA ES! BLANCA! ")
+                print(espacio, espaciov2)
+            elif x1==3:
+                print(espaciov2,espaciov2,espaciov2)
+                print("ES BOLA ES! REVENTADO! ")
+                print(espacio,espacio,espacio)
+                print(espacio, espaciov2)
+            options = input("Ingresar cualquier letra para regresar al menu:  ")
+            print(espacio, espaciov2,espacio)
+            print(espacio,espacio)
+            print(espacio,espacio,espacio,espacio)
+            print(espacio,espacio,espacio)
+            print(espacio,espacio,espacio,espacio,espacio)
+        elif a=='5':
+            print("Saliendo del programa...")
+            time.sleep(2)
+            exit()
     
 # Cuando inicia el programa para que inicie el |def| de "main()"
 if __name__ == "__main__":
